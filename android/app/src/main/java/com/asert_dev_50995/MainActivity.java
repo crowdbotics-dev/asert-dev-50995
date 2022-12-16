1500684bfdd61b797c166a751a47557ed07fd9a0
Crowdbotics <engineering@crowdbotics.com>
2022-12-16 06:03:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/asert_dev_50995/MainActivity.java
@@ -0,0 +1,15 @@
+package com.asert_dev_50995;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "asert_dev_50995";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "asert-dev-50995",
+  url: "https://asert-dev-50995.botics.co",
+  api: "https://asert-dev-50995.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
